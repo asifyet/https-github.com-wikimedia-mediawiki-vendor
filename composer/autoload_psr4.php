2448d531d23f284914a5da03ef1b6cc8f538f72e
jgleeson <jgleeson@wikimedia.org>
2021-03-04 15:03:34 +0000

    Update twig
    Bug: T271212
    Change-Id: I0b2aabcc7a9d61c2ddad8f0ca4b0899e544c797b

--- composer/autoload_psr4.php
+++ composer/autoload_psr4.php
@@ -21,6 +21,7 @@ return array(
     'Wikimedia\\' => array($vendorDir . '/wikimedia/ip-set/src', $vendorDir . '/wikimedia/ip-utils/src', $vendorDir . '/wikimedia/object-factory/src', $vendorDir . '/wikimedia/scoped-callback/src', $vendorDir . '/wikimedia/wrappedstring/src'),
     'WikiPEG\\Tests\\' => array($vendorDir . '/wikimedia/wikipeg/tests/php'),
     'WikiPEG\\' => array($vendorDir . '/wikimedia/wikipeg/src'),
+    'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Symfony\\Polyfill\\Php72\\' => array($vendorDir . '/symfony/polyfill-php72'),
     'Symfony\\Polyfill\\Php55\\' => array($vendorDir . '/symfony/polyfill-php55'),
     'Symfony\\Polyfill\\Php54\\' => array($vendorDir . '/symfony/polyfill-php54'),
